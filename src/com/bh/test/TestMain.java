50e9b633f43f01bae566b0d917c5034bcb803823
5sbread <oohogong@gmail.com>
2022-07-27 12:20:12 +0900

    Test End

--- src/com/bh/test/TestMain.java
+++ src/com/bh/test/TestMain.java
@@ -1,9 +1,17 @@
 package com.bh.test;
 
+import com.bh.util.DBConnector;
+
 public class TestMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DBConnector dbConnector = new DBConnector();
+		try {
+			dbConnector.getConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
